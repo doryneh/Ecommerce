db2a0f3858f38e942adb311f68aa39dcb0d944bd
doryneh21@gmail.com <doryneh21@gmail.com>
2021-04-02 22:18:57 +0300

    first comit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'; 
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath:"",
+    filename: "main.js",
+  },
+
+  mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    port: 1239,
+    writeToDisk: true,
+    open: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      /*  {
+        test: /\.css$/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+      }, */
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+
+    new MiniCssExtractPlugin({ filename: "css/style.css" }),
+  ],
+};
